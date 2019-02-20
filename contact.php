982d78502fab2b9105b3560c27bb3221fab6c335
mattperkins <github@mattperkins.com>
2019-02-20 01:33:33 +0000

    add title variable

--- contact.php
+++ contact.php
@@ -1,11 +1,12 @@
-<main>
-  <?php $content =  
-  
-  "<div>
+<?php 
+$title = "MySQL PHP A1 - Contact";
+$content = "<main>
+   <div>
   <h1>Contact</h1>
   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium quidem rerum architecto maiores veritatis deserunt dignissimos illum culpa quisquam reiciendis perferendis placeat laudantium nisi temporibus reprehenderit labore aut, accusantium sapiente?</p>
-</div>"
+</div>
 
-  ?>
-</main>
-<?php include "master.php" ?>
+</main>";
+
+include "master.php"; 
+?>

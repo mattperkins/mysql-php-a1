45cd4192c9d40b0c90cfdd7e88856ebb82942560
mattperkins <github@mattperkins.com>
2019-02-20 00:21:59 +0000

    scaffold

---
+++ contact.php
@@ -0,0 +1,14 @@
+<?php $contact = "
+
+<h1>Contact</h1>
+
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi dolorum, veritatis architecto maxime perspiciatis possimus molestiae explicabo mollitia minima optio quidem beatae. Totam vel dignissimos assumenda nemo, mollitia blanditiis quis.</p>
+
+"; ?>
+
+<main>
+  <?php $content = $contact ?>
+</main>
+<?php include "master.php" ?>
+
+

6074ef914cb261d5e8eb9c827c8842d1b00d61c8
mattperkins <github@mattperkins.com>
2019-02-20 01:33:43 +0000

    add title variable

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
+<?php 
+$title = "MySQL PHP A1";
+ob_start();
+?>
 <main>
-  <?php $content =  
-  
-  "<div>
+   <div>
   <h1>Home</h1>
   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium quidem rerum architecto maiores veritatis deserunt dignissimos illum culpa quisquam reiciendis perferendis placeat laudantium nisi temporibus reprehenderit labore aut, accusantium sapiente?</p>
 </div>
@@ -9,8 +11,11 @@
 <div>
   <h2>Further information</h2>
   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium quidem rerum architecto maiores veritatis deserunt dignissimos illum culpa quisquam reiciendis perferendis placeat laudantium nisi temporibus reprehenderit labore aut, accusantium sapiente?</p>
-</div>"
+</div>
 
-  ?>
 </main>
-<?php include "master.php" ?>
+
+<?php 
+$content = ob_get_clean();
+include "master.php"; 
+?>

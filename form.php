a00072e02dbf570376acbdd4ee4fb892fe11b6fd
mattperkins <github@mattperkins.com>
2019-02-20 10:01:36 +0000

    configure form method

--- form.php
+++ form.php
@@ -4,10 +4,13 @@ ob_start();
 ?>
 
 <?php 
-
-
+  if(isset($_POST['submit'])){
+    echo $_POST['email'];
+    echo $_POST['title'];
+    echo $_POST['details'];
+  }
  ?>
-<form action="" method="">
+<form action="form.php" method="POST">
   <label>Your Email</label>
   <input type="text" name="email">
   <label>Your Title</label>

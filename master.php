00f0a367c4a875b99b0f353f25b8fbdb1cf4f571
mattperkins <github@mattperkins.com>
2019-02-20 01:34:31 +0000

    add error notificatons for includes

--- master.php
+++ master.php
@@ -6,18 +6,26 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>MySQL PHP A1</title>
+  <title><?php echo $title ?></title>
   <link rel='stylesheet' href='style.css'>
 </head>
 <body>
 <header>
-  <?php include "nav.php"?>
+  <?php 
+    if((@include "nav.php") === FALSE) {
+      echo '<p class="file-missing">File missing, please contact: <a href="webmaster@dormshed.com">webmaster@dormshed.com</a></p>';
+    } 
+  ?>
 </header> 
 
 <?php echo $content; ?>
 
 <footer>
-  <?php include "footer.php"; ?>
+  <?php 
+    if((@include "footer.php") === FALSE) {
+       echo '<p class="file-missing">File missing, please contact: <a href="webmaster@dormshed.com">webmaster@dormshed.com</a></p>';
+    } 
+  ?>
 </footer>
 </body>
 </html>

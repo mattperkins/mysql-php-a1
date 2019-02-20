0bf9459ba056bf74b797f89c12091620dc3073be
mattperkins <github@mattperkins.com>
2019-02-20 02:40:39 +0000

    add list item: form

--- nav.php
+++ nav.php
@@ -2,5 +2,5 @@
   <li><a href="index.php">Home</a></li>
   <li><a href="about.php">About</a></li>
   <li><a href="contact.php">Contact</a></li>
-  <li><a href="reviews.php">Reviews</a></li>
+  <li><a href="form.php">Form</a></li>
 </ul>

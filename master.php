a2041994a94781abaf3fee4129f820c04d42b9b6
mattperkins <github@mattperkins.com>
2019-02-20 00:22:28 +0000

    scaffold layout template

---
+++ master.php
@@ -0,0 +1,23 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>MySQL PHP A1</title>
+  <link rel='stylesheet' href='style.css'>
+</head>
+<body>
+<header>
+  <?php include "nav.php"?>
+</header> 
+
+<?php echo $content; ?>
+
+<footer>
+  <?php include "footer.php"; ?>
+</footer>
+</body>
+</html>

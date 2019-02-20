072e75c59f6d16967d00fc0fe97ec7aa8d0629c5
mattperkins <github@mattperkins.com>
2019-02-20 00:21:52 +0000

    scaffold

---
+++ about.php
@@ -0,0 +1,16 @@
+<?php $about = "
+
+<h1>About</h1>
+
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi dolorum, veritatis architecto maxime perspiciatis possimus molestiae explicabo mollitia minima optio quidem beatae. Totam vel dignissimos assumenda nemo, mollitia blanditiis quis.</p>
+
+"; ?>
+
+<main>
+  <?php $content = $about ?>
+</main>
+<?php include "master.php" ?>
+
+
+
+

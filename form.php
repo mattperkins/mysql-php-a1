427f08d02e486c25952c2060d0ad29c9109d1a70
mattperkins <github@mattperkins.com>
2019-02-20 10:19:00 +0000

    validation: check for empty form field

--- form.php
+++ form.php
@@ -4,12 +4,30 @@ ob_start();
 ?>
 
 <?php 
+  
   if(isset($_POST['submit'])){
-    echo htmlspecialchars($_POST['email']);
-    echo htmlspecialchars($_POST['title']);
-    echo htmlspecialchars($_POST['details']);
-  }
+    // check email
+    if(empty($_POST['email'])){
+      echo 'An email is required <br />';
+    } else {
+      echo htmlspecialchars($_POST['email']) . '<br />';
+    }
+    // check title
+    if(empty($_POST['title'])){
+      echo 'A title is required <br />';
+    } else {
+      echo htmlspecialchars($_POST['title']) . '<br />';
+    }
+    // check email
+    if(empty($_POST['details'])){
+      echo 'Details are required <br />';
+    } else {
+      echo htmlspecialchars($_POST['details']) . '<br />';
+    }
+  } // end form POST validation
+
  ?>
+
 <form action="form.php" method="POST">
   <label>Your Email</label>
   <input type="text" name="email">

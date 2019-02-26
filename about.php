51ea447a2555ac0e927d4444ea0d03443a077c3d
mattperkins <github@mattperkins.com>
2019-02-26 13:21:10 +0000

    connect db and render data

--- about.php
+++ about.php
@@ -1,16 +1,4 @@
 <?php
-  // include in header so session persists across entire site 
-  session_start();
-
-// instantiate session
-$_SESSION["username"] = "fred4";
-// echo $_SESSION["username"];
-
-if(!isset($_SESSION["username"])){
-  echo "You are not logged in";
-} else {
-  echo "You are now logged in";
-}
 
 // connect to db
 include_once('includes/dbconfig.php');
@@ -22,28 +10,35 @@ if(!$conn){
 $sql = "SELECT * FROM posts;";
 // make query and get result
 $getData = mysqli_query($conn, $sql);
-// check for data in db table rows
+
+// view 
+$metaTitle = "MySQL PHP A1 - About";
+  ob_start();
+
+
+// check for data in db and render table rows
 $postDataCheck = mysqli_num_rows($getData);
 if($postDataCheck > 0) {
-  while($row = mysqli_fetch_assoc($getData)){
-    echo '<br />' . $row['subject'];
-  }
-}
-
-
-
-
-
-
-// view
-$metaTitle = "MySQL PHP A1 - About";
-$content = "<main>
+  while($rows = mysqli_fetch_assoc($getData)){ ?>
+    <div style="display: flex;">
+      <p style="margin-right: 3px;">
+        <?php echo htmlspecialchars($rows['subject']); ?>
+      </p>
+      <p>
+        <?php echo htmlspecialchars($rows['content']); ?>
+      </p>
+    </div>
+  <?php }
+} else { ?> 
    <div>
-  <h1>About</h1>
-  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium quidem rerum architecto maiores veritatis deserunt dignissimos illum culpa quisquam reiciendis perferendis placeat laudantium nisi temporibus reprehenderit labore aut, accusantium sapiente?</p>
-</div>
+     <?php 
+      echo "No data available"; 
+    ?>
+   </div> 
+<?php }
 
-</main>";
 
+
+$content = ob_get_clean();
 include "master.php"; 
 ?>

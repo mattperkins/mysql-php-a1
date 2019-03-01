6c7f8841d4922469f3fcc86b9052a167c99200e6
mattperkins <github@mattperkins.com>
2019-03-01 20:49:34 +0000

    update nav

--- nav.php
+++ nav.php
@@ -1,6 +1,6 @@
 <ul>
-  <li><a href="index.php">dbFromValForm</a></li>
-  <li><a href="about.php">dbRender</a></li>
+  <li><a href="index.php">dbRenderValForm</a></li>
+  <li><a href="about.php">dbRenderAddForm</a></li>
   <li><a href="contact.php">dbAdd</a></li>
   <li><a href="form.php">Validation</a></li>
 </ul>
